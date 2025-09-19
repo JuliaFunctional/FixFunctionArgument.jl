08ba374558b2c9b25cd2cba3235def9235f26256
Neven Sajko <s@purelymail.com>
2025-09-19 16:53:59 +0200

    test: add some

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,18 @@ using FixFunctionArgument
 using Test
 
 @testset "FixFunctionArgument.jl" begin
-    # Write your tests here.
+    @testset "zero-cost abstraction for type arguments" begin
+        @test (iszero ∘ sizeof ∘ Fix1)(convert, Float32)
+        @test (iszero ∘ sizeof ∘ Fix2)(typeassert, Float32)
+        @test (iszero ∘ sizeof ∘ Fix1)(Int, nothing)  # a call would throw, but that is not the point
+    end
+    @testset "`Fix1`, `Fix2`" begin
+        @test Fix1 === Fix{1}
+        @test Fix2 === Fix{2}
+    end
+    @testset "basic" begin
+        @test sin(0.3) === @inferred Fix1(sin, 0.3)()
+    end
 end
 
 using Aqua: Aqua
